bc07e2c2dcad67cf4b2ec7906f6c48abdf5034cb
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-11-09 23:18:38 -0600

    Create office

--- src/components/NewOffice.js
+++ src/components/NewOffice.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { db } from '../firebase';
 import ContainerLayout from './ContainerLayout';
 import FormOffice from './FormOffice';
 
 export default function NewOffice() {
+  const addOrEditOffice = async (officeObject) => {
+    await db
+      .collection('offices')
+      .doc()
+      .set(officeObject);
+    console.log('New office added');
+  };
+
   return (
     <>
       <ContainerLayout>
-        New Office
-        <FormOffice />
+        <FormOffice addOrEditOffice={addOrEditOffice} />
       </ContainerLayout>
     </>
   );
--- src/components/OfficeList.js
+++ src/components/OfficeList.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  ListGroup,
+  ListGroupItem,
+} from 'react-bootstrap';
 import ContainerLayout from './ContainerLayout';
 
 export default function OfficeList() {
   return (
     <>
-      <ContainerLayout>Office List</ContainerLayout>
+      <ContainerLayout>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title className="text-center">Card Title</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Ganancias: </ListGroupItem>
+                  <ListGroupItem>Empleados: </ListGroupItem>
+                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title className="text-center">Card Title</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Cras justo odio</ListGroupItem>
+                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title className="text-center">Card Title</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Cras justo odio</ListGroupItem>
+                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </ContainerLayout>
     </>
   );
 }
